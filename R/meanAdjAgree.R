StephanSeifert/SurrogateMinimalDepth

commit 4039d104f14f5f0a09f1c97135550564163febd6

fixed small bugs, e.g. that var.relations shows NA for relations to the same variables

R/meanAdjAgree.R
@@ -40,7 +40,6 @@ meanAdjAgree=function(trees,variables,allvariables,candidates,t,s.a,select.var,n
                                         list.res,
                                         index.variables)),
                           ncol=length(candidates),nrow=length(variables),byrow = TRUE)
-
   colnames(results.allvar)=candidates
   rownames(results.allvar)=variables
 
@@ -65,6 +64,7 @@ mean.index=function(i, list.res,index.variables){
   list = list.res[which(names(list.res) == index.variables[i])]
   mean.list = round(Reduce("+",list)/length(list),2)
   if (length(mean.list) > 0) {
+  mean.list[index.variables[i]] = NA
   return(mean.list)
   } else {
   return(rep(NA,length(index.variables)))

.Rhistory
@@ -1,4 +1,3 @@
-nvar=ncol(x)
 res.mir = var.select.mir(x = x,
 y = y,
 mtry = mtry,
@@ -510,3 +509,4 @@ set.seed(42)
 res = var.relations(x = x, y = y, s = 10, ntree = 100, variables = c("X1","X7"), candidates = colnames(x)[1:100], t = 5)
 res$var
 res = var.relations(x = x, y = y, s = 10, ntree = 100, variables = c("X1","X7"), candidates = colnames(x)[1:100], t = 5)
+?ranger

R/var.relations.R
@@ -95,7 +95,7 @@ var.relations = function(x = NULL, y = NULL, ntree = 500, type = "regression", s
       }
       data$status = status
       RF = ranger::ranger(data = data,dependent.variable.name = "y",num.trees = ntree,mtry = mtry,min.node.size = min.node.size,
-                          keep.inbag = TRUE, num.threads = num.threads, dependent.variable.name = "status", save.memory = save.memory,
+                          keep.inbag = TRUE, num.threads = num.threads, status.variable.name = "status", save.memory = save.memory,
                           case.weights = case.weights)
     }
     if (type == "classification" | type == "regression") {

R/var.relations.mfi.R
@@ -113,11 +113,11 @@ var.relations.mfi = function(x = NULL, y = NULL, ntree = 500, type = "regression
       }
       data$status = status
       RF = ranger::ranger(data = data,dependent.variable.name = "y",num.trees = ntree,mtry = mtry, min.node.size = min.node.size,
-                          keep.inbag = TRUE, num.threads = num.threads, dependent.variable.name = "status", save.memory = save.memory,
+                          keep.inbag = TRUE, num.threads = num.threads, status.variable.name = "status", save.memory = save.memory,
                           case.weights = case.weights, respect.unordered.factors = "partition")
 
       RF_perm = ranger::ranger(data = data_perm,dependent.variable.name = "y",num.trees = ntree,mtry = mtry,min.node.size = min.node.size,
-                          keep.inbag = TRUE, num.threads = num.threads, dependent.variable.name = "status", save.memory = save.memory,
+                          keep.inbag = TRUE, num.threads = num.threads, status.variable.name = "status", save.memory = save.memory,
                           case.weights = case.weights, respect.unordered.factors = "partition")
     }
     if (type == "classification" | type == "regression") {
@@ -175,22 +175,19 @@ var.relations.mfi = function(x = NULL, y = NULL, ntree = 500, type = "regression
   adj.agree.perm = rel_perm$surr.res
   diag(adj.agree) = diag(adj.agree.perm) = 1
 
-
   if(anyNA(adj.agree)) {
-    no.na = length(which(is.na(adj.agree)))
-    warning(paste0(no.na," relations were not calculated for the original variables because not all variables are used in a primary split.
+    no.na = length(which(rowSums(is.na(adj.agree)) != 0 ))
+    warning(paste0("Relations for ", no.na, " original variables were not calculated because they were never used as a primary split.
             Affected relations are set to 0. "))
     adj.agree[which(is.na(adj.agree))] = 0
   }
 
   if(anyNA(adj.agree.perm)) {
-    no.na = length(which(is.na(adj.agree.perm)))
-    warning(paste0(no.na," relations were not calculated for the permuted variables because not all variables are used in a primary split.
+    no.na = length(which(rowSums(is.na(adj.agree.perm)) != 0 ))
+    warning(paste0("Relations for ", no.na, " permuted variables were not calculated because they were not used as a primary split.
             Affected relations are set to 0. "))
     adj.agree.perm[which(is.na(adj.agree.perm))] = 0
   }
-
-
   adj.agree.corr = adj.agree - adj.agree.perm[1:nvar,1:nvar]
   diag(adj.agree.corr) =  diag(adj.agree) = diag(adj.agree.perm) =  NA
 

AGSeifert/RFSurrogates

commit d9ed74ba0f7e6c1ac5aa2ea1d065e2b6ff536e3d

fix: `mean.index()` being padded with trailing NA over length(variables)
It was intended to set the index of the variable with itself to be NA.
As written, this would set the index of the variable among all variables in the data set to be NA, thus creating longer vectors when the index of that variable exceeded the length of `variables`.
Fixes bug introduced in https://github.com/StephanSeifert/SurrogateMinimalDepth/commit/4039d104f14f5f0a09f1c97135550564163febd6

R/meanAdjAgree.R
@@ -67,7 +67,7 @@ mean.index <- function(i, list.res, index.variables) {
   list <- list.res[which(names(list.res) == index.variables[i])]
   mean.list <- round(Reduce("+", list) / length(list), 2)
   if (length(mean.list) > 0) {
-    mean.list[index.variables[i]] <- NA
+    mean.list[i] <- NA
     return(mean.list)
   } else {
     return(rep(NA, length(index.variables)))